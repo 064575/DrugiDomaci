e5c9b47a428eda95b423bdf6071decb7786a570f
064575 <aleksabajovicbg3@gmail.com>
2022-01-05 13:06:13 +0100

    Kreiran model i product kao i migracija seeder i factory

---
+++ app/Models/Product.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'color_id', 'memory', 'storage', 'display_diagonal', 'os'];
+}
